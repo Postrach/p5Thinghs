a3634ac18d02d4e2fada31cc6d73387a75ac7680
Postrach <34251042+Postrach@users.noreply.github.com>
2019-03-01 09:59:13 +0100

    Update randomThing.js

--- p5/empty-example/randomThing.js
+++ p5/empty-example/randomThing.js
@@ -2,23 +2,21 @@
 
 function setup() { 
 	createCanvas(400, 400);	
-	frameRate(1);
+	frameRate(10);
 	} 
 function draw() { 
 	background(0);		
 	let randWidth = random(0, width);	
 	let randHeight = random(0, height);	
-	translate(mouseX, mouseY);			
-	stroke(120);	
+	translate(mouseX, mouseY);
+	stroke(mouseX/width + mouseY/height);	
 	noFill();		
 	beginShape();	
 	for(let a = 0; a< TWO_PI; a+=0.001){		
 		let r = 100;		
 		let rand = random(10);		
 		let x = r * cos(a) * rand;		
-		let y = r * sin(a) * rand;				
-		let randStroke = random(100,250);		
-		stroke(randStroke);		
+		let y = r * sin(a) * rand;	
 		vertex(x,y);	
 		}	
 	endShape();	
