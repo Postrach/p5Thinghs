c75c1e5db49bbe307a260f218a925122cb16a4f9
Postrach <34251042+Postrach@users.noreply.github.com>
2019-03-01 12:39:37 +0100

    Added player interactions

--- p5/GameOfLife/sketch.js
+++ p5/GameOfLife/sketch.js
@@ -1,12 +1,45 @@
 let columns;
 let rows;
-let cellSize = 10;
+let cellSize = 20;
 let grid;
 let generation = 0;
+let playing = true;
+
+let frameRateSlider;
+let cellSizeSlider;
+let generationInfo;
+
+var div;
+let stopButtona;
+
+let time = 0;
 
 function setup() {
-  createCanvas(800, 800);
-	frameRate(5);
+	let canvas = createCanvas(800, 500);
+	canvas.position(width/2, height/2);
+	frameRate(20);
+	textSize(15);
+
+	resetSketch()
+	
+	frameRateSlider = createSlider(-10, -1, -5);
+	var divFRate = createDiv('speed').size(100,50);
+
+	cellSizeSlider = createSlider(10, 30, 12);
+	var divCSize = createDiv('cell').size(100,50);
+
+	let button = createButton("reset");
+	button.mousePressed(resetSketch);
+
+	stopButton = createButton("pause");
+	stopButton.mousePressed(stop);
+
+	div = createDiv('').size(100,100);
+}
+
+function resetSketch(){
+	//cellSize = cellSizeSlider.value();
+	
 
 	columns = width / cellSize;
 	rows = height / cellSize;
@@ -14,32 +47,52 @@ function setup() {
 	grid = makeGrid();
 	//fillGridWithRandomStates();
 	fillGridGlider();
+
+	generation = 0;
 }
 
 function draw() {
 	background(255);
 
+	speed = frameRateSlider.value();
+	//frameRate(speed);
+
+	div.html('Generation: ' + generation);
 
 	//draw grid
-	for(let i = 0; i < columns; i++){
-		for(let j = 0; j < rows; j++){
+	for(let i = 1; i < columns - 1; i++){
+		for(let j = 1; j < rows - 1; j++){
 			if(grid[i][j] == 1){
 				fill(0);
-			}else{
+			}else if(grid[i][j] == 0){
 				fill(255);
 			}
 			rect(i * cellSize, j * cellSize, cellSize - 1, cellSize - 1);
 		}
 	}
 
-	
-	grid = createNewGeneration();
+	if(millis() > time && playing == true){
+		time = millis() + speed * -200;
+		grid = createNewGeneration();
 
-
-	console.log("Generation: " + generation);
-	generation++;
+		//console.log("Generation: " + generation);
+		generation++;
+	}
 }
 
+function stop(){
+	if(playing == true){
+		playing = false;
+		stopButton.html("play");
+		//noLoop();
+	}else{
+		//loop();
+		playing = true;
+		stopButton.html("pause");
+	}
+}
+
+//create empty 2d array
 function makeGrid(){
 	let arr = new Array(columns);
 	 for (let i = 0; i < arr.length; i++) {
@@ -48,12 +101,13 @@ function makeGrid(){
 	 return arr;
 }
 
+//creates new generation based on previous onea
 function createNewGeneration(){
 	let newGrid = makeGrid();
 	for(let i = 0; i < columns; i++){
 		for(let j = 0; j < rows; j++){
 			if(i==0 || j == 0 || i == columns -1 || j == rows -1){
-				newGrid[i][j] = 0;
+				newGrid[i][j] = 3;
 			}else{
 				let count = checkNeighbors(i,j);
 				let state = grid[i][j];
@@ -71,38 +125,7 @@ function createNewGeneration(){
 	return newGrid;
 }
 
-//for testing 
-function fillGridWithRandomStates(){
-	for(let i =0; i< columns; i++){
-		for(let j = 0; j < rows; j++){
-			if(i==0 || j == 0 || i == columns -1 || j == rows -1){
-				grid[i][j] = 0;
-			}else{
-				let rand = floor(random(0, 2));
-				if(rand == 1){
-					grid[i][j] = 1;
-				}
-			}
-		}
-	}
-}
-
-function fillGridGlider(){
-	for(let i = 0; i < columns; i++){
-		for(let j = 0; j < rows; j++){
-			grid[i][j] = 0;
-		}
-	}
-
-	let x = columns/2;
-	let y = rows/2;
-	grid[x][y+1] = 1;
-	grid[x][y-1] = 1;
-	grid[x+1][y] = 1;
-	grid[x-1][y+1] = 1;
-	grid[x+1][y+1] = 1;
-}
-
+//checks the amount of living neighbors
 function checkNeighbors(x, y){
 	let neighborCount = 0;
 	for(let i=-1; i <= 1; i++){
@@ -117,4 +140,62 @@ function checkNeighbors(x, y){
 		}
 	}
 	return neighborCount;
+}
+
+//----------------------------------------------//
+//----------------Grid filling------------------//
+//----------------------------------------------//
+
+//fills edges
+function fillEdges(){
+	for(let i =0; i< columns; i++){
+		for(let j = 0; j < rows; j++){
+			if(i==0 || j == 0 || i == columns -1 || j == rows -1){
+				grid[i][j] = 3;
+			}
+		}
+	}
+}
+
+//fills grid with random states
+function fillGridWithRandomStates(){
+	for(let i = 1; i< columns - 1; i++){
+		for(let j = 1; j < rows - 1; j++){
+			let rand = floor(random(0, 2));
+			if(rand == 1){
+				grid[i][j] = 1;
+			}
+			
+		}
+	}
+}
+
+//fills grid in a way to obtain a "slider"
+function fillGridGlider(){
+	for(let i = 1; i < columns - 1; i++){
+		for(let j = 1; j < rows - 1; j++){
+			grid[i][j] = 0;
+		}
+	}
+
+	let x = columns/2;
+	let y = rows/2;
+	grid[x][y+1] = 1;
+	grid[x][y-1] = 1;
+	grid[x+1][y] = 1;
+	grid[x-1][y+1] = 1;
+	grid[x+1][y+1] = 1;
+}
+
+function mousePressed(){
+	console.log(mouseX + ", " + mouseY);
+	if(mouseX > 1 && mouseX < width -1 && mouseY > 1 && mouseY < height -1){
+		let x = floor(mouseX / cellSize);
+		let y = floor(mouseY / cellSize);
+		if(grid[x][y] == 0){
+			grid[x][y] = 1;
+		}else{
+			grid[x][y] = 0;
+		}
+	}
 }
